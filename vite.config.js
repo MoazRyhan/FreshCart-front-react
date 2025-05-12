6ef3456c67d709acf0b0ed0e888d0d9be98ebd8b
mouis666666 <moaz666666@outlook.com>
2025-05-12 13:40:08 +0300

    ll

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  base: '/FreshCart-front-react/', // ✅ your GitHub repo name here
   plugins: [react()],
-})
+  base: '/FreshCart-front-react/', // this MUST match your GitHub repo name
+});
