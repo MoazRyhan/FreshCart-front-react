aa6daee49aea6e65c3e8ce98a22e7cf181702588
mouis666666 <moaz666666@outlook.com>
2025-05-12 13:17:09 +0300

    f

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
 
   <BrowserRouter>
-  <App  basename="/FreshCart-front-react"/>
+  <App/>
   </BrowserRouter>
     
 
