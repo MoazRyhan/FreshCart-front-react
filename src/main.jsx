335e63665e1cd8adb7c4457badecdf91bc4f87dd
mouis666666 <moaz666666@outlook.com>
2025-05-12 13:15:01 +0300

    tt

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
 
-  
-    <App  basename="/FreshCart-front-react"/>
+  <BrowserRouter>
+  <App  basename="/FreshCart-front-react"/>
+  </BrowserRouter>
+    
 
 )
